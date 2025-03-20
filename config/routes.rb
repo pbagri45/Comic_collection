bfcfb18f801573e0e2f7f96afaa95f31f38ab8b6
pbagri45 <pbagri@rrc.ca>
2025-03-19 23:21:48 -0500

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
+  resources :comics do
+    resources :reviews, only: [:new, :create]
+  end
+  root "comics#index"
+   
 end
