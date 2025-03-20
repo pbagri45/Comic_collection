a02cc3259ee9ed7c0cafe8db3db796a920e854cb
pbagri45 <pbagri@rrc.ca>
2025-03-19 23:24:33 -0500

    Added search comic layout

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -10,4 +10,12 @@ class ComicsController < ApplicationController
 
   def edit
   end
+
+  def index
+    if params[:search]
+      @comics = Comic.where("title LIKE ?", "%#{params[:search]}%")
+    else
+      @comics = Comic.all
+    end
+  end
 end
