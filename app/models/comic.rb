58088623fb47a1dfae0dfa5e6bff87a147cec0bc
pbagri45 <pbagri@rrc.ca>
2025-03-19 23:33:24 -0500

    Mapping

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -6,4 +6,12 @@ class Comic < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true  
+
+  geocoded_by :address
+  after_validation :geocode, if: ->(obj) { obj.address.present? && obj.address_changed? }
+
+  def address
+    # Combine address fields (e.g., street, city, state)
+    [street, city, state].compact.join(', ')
+  end
 end
