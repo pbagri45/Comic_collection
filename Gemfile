83087d4fe246f4843d718cf13e50a2bcee7f00b7
pbagri45 <pbagri@rrc.ca>
2025-03-19 23:30:29 -0500

    Adding hierarchical search

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem 'will_paginate', '~> 3.3'
--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -18,4 +18,15 @@ class ComicsController < ApplicationController
       @comics = Comic.all
     end
   end
+
+  def index
+    @comics = Comic.paginate(page: params[:page], per_page: 10)
+  end
+  def index
+    @comics = Comic.all
+    @comics = @comics.where("title LIKE ?", "%#{params[:title]}%") if params[:title].present?
+    @comics = @comics.where(publisher_id: params[:publisher_id]) if params[:publisher_id].present?
+    @comics = @comics.where(series_id: params[:series_id]) if params[:series_id].present?
+    @comics = @comics.paginate(page: params[:page], per_page: 10)
+  end
 end
